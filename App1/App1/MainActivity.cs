4ea12b564cf9106dd7d671b76d1313b51a910f51
Mait Kaasik <[mait.kaasik@tptlive.ee]>
2019-10-05 12:45:17 +0300

    fixes

--- App1/App1/MainActivity.cs
+++ App1/App1/MainActivity.cs
@@ -3,6 +3,7 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using System;
 
 namespace App1
 {
@@ -16,11 +17,23 @@ namespace App1
             SetContentView(Resource.Layout.first_layout1);
 
             var textview = FindViewById<TextView>(Resource.Id.textView1);
+            var textfield1 = FindViewById<EditText>(Resource.Id.editText1);
+            var textfield2 = FindViewById<EditText>(Resource.Id.editText1);
             var button = FindViewById<Button>(Resource.Id.button1);
+
+            //double a = Convert.ToDouble(textfield1);
+            //double b = Convert.ToDouble(textfield2);
+            var arv1 = int.Parse(textfield1.Text);
+            var arv2 = int.Parse(textfield2.Text);
+            var result = 0;
+
 
             button.Click += delegate
             {
-                textview.Text = "It works";
+
+                result = arv1 + arv2;
+                textview.Text = "result = "+result;
+                
             };
         }
     }
