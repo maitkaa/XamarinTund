a3530a8443a07fd0c55ce41c4766c5a5f8be639b
Mait Kaasik <mait.kaasik@tptlive.ee>
2019-11-02 10:33:05 +0200

    added EditText

--- SecondApp/SecondApp/MainActivity.cs
+++ SecondApp/SecondApp/MainActivity.cs
@@ -3,7 +3,7 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
-
+using Android.Content;
 namespace SecondApp
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
@@ -15,6 +15,21 @@ namespace SecondApp
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            var toSecondActivityButton = FindViewById<Button>(Resource.Id.button1);
+            var editText = FindViewById<EditText>(Resource.Id.editText1);
+
+
+            toSecondActivityButton.Click += delegate
+            {
+                
+                var text = editText.Text;
+                var intent = new Intent(this,typeof(SecondActivity));
+                intent.PutExtra("edittextvalue", text);
+                StartActivity(intent);
+            
+            };
+
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
---
+++ SecondApp/SecondApp/SecondActivity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace SecondApp
+{
+    [Activity(Label = "SecondActivity")]
+    public class SecondActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.second_layout);
+            var text = Intent.GetStringExtra("edittextvalue");
+
+
+            var textview = FindViewById<TextView>(Resource.Id.textView1);
+            textview.Text = text;
+
+
+
+        }
+    }
+}
