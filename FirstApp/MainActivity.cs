d42c6763208b04d67506d034469bd863d511aee7
Mait Kaasik <mait.kaasik@tptlive.ee>
2019-12-05 23:20:59 +0200

    fixed Calc app

---
+++ FirstApp/MainActivity.cs
@@ -0,0 +1,62 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+
+namespace FirstApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        
+        protected double result;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            
+            //Result
+            var textview = FindViewById<TextView>(Resource.Id.textView1);
+            //Userinput 1 and 2 In EditText XML must be android:inputType="numberDecimal" 
+            var textfield1 = FindViewById<EditText>(Resource.Id.editText1);
+            var textfield2 = FindViewById<EditText>(Resource.Id.editText2);
+            //Calculate and Clear buttons
+            var button = FindViewById<Button>(Resource.Id.button1);
+            var button2 = FindViewById<Button>(Resource.Id.button2);
+
+                button.Click += delegate
+                {
+                    var inputtext = textfield1.Text.ToString();
+
+                    var inputtext2 = textfield2.Text.ToString();
+
+                    result = Convert.ToDouble(inputtext) + Convert.ToDouble(inputtext2);
+
+                    textview.Text = "Result is " + result.ToString();
+
+                };
+            
+           // Clear textfields and restore Textview original value
+            button2.Click += delegate
+            {
+                textfield1.Text = "";
+                textfield2.Text = "";
+                textview.Text = "Super Calculator";
+
+            };
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+      
+    }
+}
--- FourthApp/FourthApp/MainActivity.cs
+++ FourthApp/FourthApp/MainActivity.cs
@@ -18,10 +18,7 @@ namespace FourthApp
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            base.OnCreate(savedInstanceState);
-            AppCenter.Start("8a11b210-a43a-490a-9a79-971c42b8b3e6",
-                   typeof(Analytics), typeof(Crashes));
-      
+           
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
@@ -44,48 +41,7 @@ namespace FourthApp
 
             };
             
-            var countries = new String[] {
-               "Afghanistan","Albania","Algeria","American Samoa","Andorra",
-    "Angola","Anguilla","Antarctica","Antigua and Barbuda","Argentina",
-    "Armenia","Aruba","Australia","Austria","Azerbaijan",
-    "Bahrain","Bangladesh","Barbados","Belarus","Belgium",
-    "Belize","Benin","Bermuda","Bhutan","Bolivia",
-    "Bosnia and Herzegovina","Botswana","Bouvet Island","Brazil","British Indian Ocean Territory",
-    "British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi",
-    "Cote d'Ivoire","Cambodia","Cameroon","Canada","Cape Verde",
-    "Cayman Islands","Central African Republic","Chad","Chile","China",
-    "Christmas Island","Cocos (Keeling) Islands","Colombia","Comoros","Congo",
-    "Cook Islands","Costa Rica","Croatia","Cuba","Cyprus","Czech Republic",
-    "Democratic Republic of the Congo","Denmark","Djibouti","Dominica","Dominican Republic",
-    "East Timor","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea",
-    "Estonia","Ethiopia","Faeroe Islands","Falkland Islands","Fiji","Finland",
-    "Former Yugoslav Republic of Macedonia","France","French Guiana","French Polynesia",
-    "French Southern Territories","Gabon","Georgia","Germany","Ghana","Gibraltar",
-    "Greece","Greenland","Grenada","Guadeloupe","Guam","Guatemala","Guinea","Guinea-Bissau",
-    "Guyana","Haiti","Heard Island and McDonald Islands","Honduras","Hong Kong","Hungary",
-    "Iceland","India","Indonesia","Iran","Iraq","Ireland","Israel","Italy","Jamaica",
-    "Japan","Jordan","Kazakhstan","Kenya","Kiribati","Kuwait","Kyrgyzstan","Laos",
-    "Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg",
-    "Macau","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands",
-    "Martinique","Mauritania","Mauritius","Mayotte","Mexico","Micronesia","Moldova",
-    "Monaco","Mongolia","Montserrat","Morocco","Mozambique","Myanmar","Namibia",
-    "Nauru","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand",
-    "Nicaragua","Niger","Nigeria","Niue","Norfolk Island","North Korea","Northern Marianas",
-    "Norway","Oman","Pakistan","Palau","Panama","Papua New Guinea","Paraguay","Peru",
-    "Philippines","Pitcairn Islands","Poland","Portugal","Puerto Rico","Qatar",
-    "Reunion","Romania","Russia","Rwanda","Sqo Tome and Principe","Saint Helena",
-    "Saint Kitts and Nevis","Saint Lucia","Saint Pierre and Miquelon",
-    "Saint Vincent and the Grenadines","Samoa","San Marino","Saudi Arabia","Senegal",
-    "Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands",
-    "Somalia","South Africa","South Georgia and the South Sandwich Islands","South Korea",
-    "Spain","Sri Lanka","Sudan","Suriname","Svalbard and Jan Mayen","Swaziland","Sweden",
-    "Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","The Bahamas",
-    "The Gambia","Togo","Tokelau","Tonga","Trinidad and Tobago","Tunisia","Turkey",
-    "Turkmenistan","Turks and Caicos Islands","Tuvalu","Virgin Islands","Uganda",
-    "Ukraine","United Arab Emirates","United Kingdom",
-    "United States","United States Minor Outlying Islands","Uruguay","Uzbekistan",
-    "Vanuatu","Vatican City","Venezuela","Vietnam","Wallis and Futuna","Western Sahara",
-    "Yemen","Yugoslavia","Zambia","Zimbabwe" };
+            
    
             listView.Adapter = new basicAdapter(this, carList);
 
